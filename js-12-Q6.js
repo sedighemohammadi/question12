325cee02f9101ca6247c1ee4f299d6bda073f34e
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-08 09:00:16 +0330

    6

---
+++ js-12-Q6.js
@@ -0,0 +1,14 @@
+// ? Question-9:Follow the steps below:
+// TODO-1: Write an object and add two properties: name and age.
+// ToDo-2: Write a method for this object; this method must print 'string' in the console. Like => `Hello, my name is {name for example Jasem}, and I am {age for example 120} years old.`
+// hint: To access properties in the method you must use "this" keyword.
+// !Answer:
+const student = {
+    name:'jasem',
+    age:120 ,
+    mathod :function(){
+        console.log(`hello ! my name is ${this.name} , and i am ${this.age}`);
+        
+    }
+}
+student.method();
