1d085ef4d29080cb188c633ed8905de6fedb9439
sedighemohammadi <sedighemohammadi387@gmail.com>
2024-12-08 08:59:57 +0330

    3

---
+++ js-12-Q3.js
@@ -0,0 +1,20 @@
+// ? Question-3: follow these steps:
+// todo-1: Write a function that receives an array of numbers
+// todo-2: In The function that you created in todo-1, you should create two functions.
+// todo-3: the first function displays the first half of the array, and the second function displays the second half of the array..
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/*=> output
+   [ 1, 2, 3, 4, 5 ]
+   [ 6, 7, 8, 9, 10 ]
+ */
+
+// !Answer:
+function favorite(arr) {
+  const middle = arr.length / 2 
+  console.log(arr.slice(0 , middle));
+  console.log(arr.slice(middle));
+
+}
+favorite(numbers)
